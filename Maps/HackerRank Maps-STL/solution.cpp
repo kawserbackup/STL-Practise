d28a9fcd90296640b5b6534f5d043cf8323616d7
sakibriyad2 <69283513+sakibriyad2@users.noreply.github.com>
2021-05-06 14:47:09 +0600

    Update solution.cpp

--- Maps/HackerRank Maps-STL/solution.cpp
+++ Maps/HackerRank Maps-STL/solution.cpp
@@ -1,3 +1,5 @@
+//find er kaj bujhtesina
+
 #include<iostream>
 using namespace std;
 #include<map>
@@ -12,6 +14,19 @@ int main ()
     cout<<length<<endl;
     m.erase("val"); //Erases the pair from the map where the key_type is val.
     cout<<length<<endl;/*ei line print korar por 1 asar kotha chilo coz delete kore dici but aslona keno */
+    map<string,int>::iterator itr; //declared the iterator for using on the find function
+    itr=m.find("Maps");
+
+    /*
+    The find() member function searches for the first element that has the appropriate key and returns
+    its iterator position. If no such element is found, find() returns end() of the container. You can’t
+    use the find() member function to search for an element that has a certain value
+    */
+
+    //first mane holo key and second mane holo value.
+
+    cout<<m.find("9")->second<<endl; //If Maps is not present as the key value then itr==m.end().
+    cout<<m.find("hello")->second<<endl;
     return 0;
 }
 
